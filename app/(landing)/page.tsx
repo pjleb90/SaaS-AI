34e1a38ca5738801fc3b9b689312b8de4c384eb8
pjleb90 <pjleb90@gmail.com>
2023-08-26 16:04:11 -0400

    created and styled landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,25 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
-import Link from "next/link";
 
 const LandingPage = () => {
-    return (
-        <div>
-            Landing Page (Unprotected)
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
-        </div>
-      );
+  return (
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+   );
 }
 
 export default LandingPage;
